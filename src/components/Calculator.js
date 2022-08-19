67d6141b730329308243a7b3049b3b12a2dadf7a
Basit Ali <allahbadshah148@gmail.com>
2022-08-20 00:39:04 +0500

    Adding hook functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,91 +1,82 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable */
-import React from "react";
+import React,{useState} from "react";
 import calculate from "./Logic/calculate";
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator=()=>  {
+  const [state, calc] = useState(0);
+  const { total, next, operation } = state;
 
-  handleclick = (e) => {
+ const handleclick = (e) => {
     const { innerText } = e.target;
-    const result = calculate(this.state, innerText);
-    this.setState(result);
+   calc(calculate(state, innerText));
   };
-  render() {
-    const { total, next, operation } = this.state;
     return (
       <div className="calculator">
         <div id="display" className="display">
           {total || next}
         </div>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           AC
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           +/-
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           %
         </button>
-        <button type="button" onClick={this.handleclick} className="orange">
+        <button type="button" onClick={handleclick} className="orange">
           ÷
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           7
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           8
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           9
         </button>
-        <button type="button" onClick={this.handleclick} className="orange">
+        <button type="button" onClick={handleclick} className="orange">
           x
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           4
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           5
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           6
         </button>
-        <button type="button" onClick={this.handleclick} className="orange">
+        <button type="button" onClick={handleclick} className="orange">
           -
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           1
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           2
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           3
         </button>
-        <button type="button" onClick={this.handleclick} className="orange">
+        <button type="button" onClick={handleclick} className="orange">
           +
         </button>
-        <button type="button" onClick={this.handleclick} className="span2">
+        <button type="button" onClick={handleclick} className="span2">
           0
         </button>
-        <button type="button" onClick={this.handleclick}>
+        <button type="button" onClick={handleclick}>
           .
         </button>
-        <button type="button" onClick={this.handleclick} className="orange">
+        <button type="button" onClick={handleclick} className="orange">
           =
         </button>
       </div>
     );
   }
-}
+
 
 export default Calculator;
