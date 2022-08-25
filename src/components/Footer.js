830d77c833d0837458dbd1c971da88ab41b81626
Basit Ali <allahbadshah148@gmail.com>
2022-08-25 10:11:29 +0500

    Adding styles and routes

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer id="footer">
+    <p>Basit Ali © 2022 All rights reserved.</p>
+  </footer>
+);
+export default Footer;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Quotes.css';
+
+const Quotes = () => (
+  <section className="Quotes">
+    <p>
+      <p>
+        &quot;Life is a math equation, in order to gain the most, you have to
+        know how to convert negatives into positives.&quot;
+      </p>
+      <span>by Unkwnown</span>
+    </p>
+  </section>
+);
+export default Quotes;
